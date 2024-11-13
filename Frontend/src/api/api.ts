8e30cad4de447a8a9950969084be17260db43eac
disisVu <102909483+disisVu@users.noreply.github.com>
2024-11-13 10:21:07 +0700

    Frontend: update API base url

--- Frontend/src/api/api.ts
+++ Frontend/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const axiosPrivate = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://ia04-jwtauthentication-backend-sypl.onrender.com',
   headers: {
     'Content-Type': 'application/json'
   }
